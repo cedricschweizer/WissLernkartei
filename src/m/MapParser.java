6023ed3f94adcb64d2b3955d2be13ebd7bab30b5
vividhues <beer.thierry@bluewin.ch>
2018-06-21 08:57:40 +0200

    RE::Commit

--- src/m/MapParser.java
+++ src/m/MapParser.java
@@ -1,5 +1,12 @@
 package m;
 
-public class MapParser {
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
+public class MapParser {
+    private PrintWriter writer;
+
+    public MapParser(String fileName) throws FileNotFoundException {
+        this.writer = new PrintWriter(fileName);
+    }
 }
