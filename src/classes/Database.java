3924a7ed6965bba0a32cb01c27c7e9d150709050
Developer <cedricschweizer1@gmail.com>
2018-06-28 11:26:22 +0200

    database changes

--- src/classes/Database.java
+++ src/classes/Database.java
@@ -150,7 +150,7 @@ public class Database {
     public void deleteDatabase() {
         try (Connection connection = DriverManager.getConnection(url);
              Statement stmt = connection.createStatement()) {
-            stmt.execute("DROP DATABASE IF EXISTS WissLK CASCADE ;");
+            stmt.execute("DROP DATABASE IF EXISTS WissLK;");
             System.out.println("Successfully dropped database!");
         } catch (SQLException e) {
             System.out.println("Es ist ein Fehler aufgetreten! Bitte kontaktieren Sie Ihre Eltern!");
